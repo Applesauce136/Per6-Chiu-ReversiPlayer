4d6bffa1ad0ebdad9286f3c24155cfaa06d026fe
Harrison Chiu <harrison.chiu.136@gmail.com>
2014-06-06 10:45:57 -0400

    Moved the sketch's output into the same directory as emacs processing-mode's output thing or something

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 PROCLOC="/home/sorbet1331/processing-2.2.1/processing-java "
 SKETCH="--sketch=src "
-OUTPUT="--output=output "
+OUTPUT="--output=src/output "
 FORCE="--force "
 RUN="--run "
 $PROCLOC$SKETCH$OUTPUT$FORCE$RUN
