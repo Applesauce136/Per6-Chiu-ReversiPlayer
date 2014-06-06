0300074aa7fa95a8f36f97299ac05aa31e192b6a
Harrison Chiu <harrison.chiu.136@gmail.com>
2014-06-06 10:45:12 -0400

    Organized some code; removed the command-line Driver

--- src/output/source/src.java
+++ src/output/source/src.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class src extends PApplet {
+
 private int 
     squareSize = 80,
     rows = 8,
@@ -5,7 +21,7 @@ private int
 private Tree game;
 private Thread build;
 
-void setup()
+public void setup()
 {
     size(squareSize * 8, squareSize * 8);
     game = new Tree(1);
@@ -13,7 +29,7 @@ void setup()
     redraw();
 }
 
-void draw()
+public void draw()
 {
     background(0, 150, 0);
     int parts = 8;
@@ -40,7 +56,7 @@ void draw()
     build.start();
 }
 
-void mouseClicked()
+public void mouseClicked()
 {
     int row = mouseX / squareSize;
     int col = mouseY / squareSize;
@@ -51,7 +67,7 @@ void mouseClicked()
 	}
 }
 
-void round(Move move)
+public void round(Move move)
 {
     try 
 	{
@@ -64,4 +80,13 @@ void round(Move move)
     build.interrupt();
     game.play(move);
     redraw();
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "src" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
