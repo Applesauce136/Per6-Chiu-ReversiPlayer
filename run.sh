2731865361fce9037a69973bc0cfd1ce28362723
Harrison Chiu <harrison.chiu.136@gmail.com>
2014-06-09 08:32:12 -0400

    I just put processing into the repo because it's more convenient

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PROCLOC="/home/sorbet1331/processing-2.2.1/processing-java "
+PROCLOC="./processing-2.2.1/processing-java "
 SKETCH="--sketch=src "
 OUTPUT="--output=src/output "
 FORCE="--force "
